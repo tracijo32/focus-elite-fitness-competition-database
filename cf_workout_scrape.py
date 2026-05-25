690cca01ab01712ab0b8cdfa3d0f1ecfe0b23940
Traci Johnson <traci.johnson.32@gmail.com>
2026-05-25 15:44:43 -0500

    added regionals, fixed games year that only pulled flow

--- cf_workout_scrape.py
+++ cf_workout_scrape.py
@@ -2,44 +2,71 @@ import requests
 from bs4 import BeautifulSoup
 import json
 from pathlib import Path
+from typing import Callable
 
-def fetch_games_workouts():
-    years = range(2007,MAX_YEAR+1)
+def fetch_games_workouts(year: int):
     days_of_week = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
-    workouts_all = []
-    for year in years:
-        for slug in ['games','finals']:
-            try:
-                url = f'https://games.crossfit.com/workouts/{slug}/{year}'
-                r = requests.get(url)
-                r.raise_for_status()
-                soup = BeautifulSoup(r.text, 'html.parser')
-                break
-            except Exception as e:
-                pass
+    for slug in ['games','finals']:
+        try:
+            url = f'https://games.crossfit.com/workouts/{slug}/{year}'
+            r = requests.get(url)
+            r.raise_for_status()
+            soup = BeautifulSoup(r.text, 'html.parser')
+            break
+        except Exception as e:
+            pass
 
-        events = soup.find('ol', attrs={'class':'events'})
-        if events is None:
-            continue
-        cols = events.find_all('li', attrs={'class':'panel'})
+    if year == 2021:
+        return parse_games_2021(soup)
 
-        workouts = []
-        for col in cols:
-            class_values = col.attrs['class']
-            out = {
-                'year': year,
-                'workout_name': col.find('header').text.strip(),
-                'description': col.find('div',attrs={'class':'description'}).text.strip()
+    events = soup.find('ol', attrs={'class':'events'})
+    if events is None:
+        return []
+    cols = events.find_all('li', attrs={'class':'panel'})
+
+    workouts = []
+    for i, col in enumerate(cols):
+        class_values = col.attrs['class']
+        out = {
+            'year': year,
+            'seq': i+1,
+            'workout_name': col.find('header').text.strip(),
+            'description': col.find('div',attrs={'class':'description'}).text.strip()
+        }
+        for class_value in class_values:
+            for day in days_of_week:
+                if day in class_value:
+                    out['date'] = class_value
+                    break
+        workouts.append(out)
+    return workouts
+
+def parse_games_2021(soup: BeautifulSoup):
+    comp_days = soup.find_all('h2',attrs={'class':'calendar-heading'})
+
+    workouts = []
+    for day in comp_days:
+        w = {}
+        name = None
+        for s in day.next_siblings:
+            if s.text.startswith('Event'):
+                name = s.text
+            elif name is not None:
+                w.setdefault(name,[]).append(s.text)
+
+        w = [
+            {
+                'year': 2021,
+                'date': day.text,
+                'seq': i,
+                'workout_name': k,
+                'description': ''.join(v)
             }
-            for class_value in class_values:
-                for day in days_of_week:
-                    if day in class_value:
-                        out['date'] = class_value
-                        break
-            workouts.append(out)
-        workouts_all.extend(workouts)
+            for i, (k,v) in enumerate(w.items())
+        ]
+        workouts.extend(w)
 
-    return workouts_all
+    return workouts
 
 def fetch_stage_workout(
     stage: str,
@@ -66,10 +93,11 @@ def fetch_stage_workout(
 
     return workouts
 
-def fetch_open_workout_old(
-    year: int
+def fetch_stage_workout_old(
+    year: int,
+    stage: str
 ):
-    url = f"https://games.crossfit.com/workouts/open/{year}"
+    url = f"https://games.crossfit.com/workouts/{stage}/{year}"
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
 
@@ -88,25 +116,18 @@ def fetch_open_workout_old(
 
     return workouts
     
-def fetch_open_workouts():
-    workouts_all = []
-    for year in range(2011,MAX_YEAR+1):
-        if year <= 2016:
-            workouts = fetch_open_workout_old(year)
-        else:
-            workouts = fetch_stage_workout('open', year)
-        workouts_all.extend(workouts)
-    return workouts_all
+def fetch_open_workouts(year: int):
+    workouts = []
+    if year <= 2016:
+        workouts = fetch_stage_workout_old(year, 'open')
+    else:
+        workouts = fetch_stage_workout('open', year)
+    return workouts
 
-def fetch_quarterfinals_workouts():
-    years = range(2021,MAX_YEAR+1)
-    workouts_all = []
-    for year in years:
-        if year == 2025:
-            continue
-        workouts = fetch_stage_workout('quarterfinalsindividual', year)
-        workouts_all.extend(workouts)
-    return workouts_all
+def fetch_quarterfinals_workouts(year: int):
+    if year == 2025:
+        return None
+    return fetch_stage_workout('quarterfinalsindividual', year)
 
 def fetch_semis_2022():
     url = "https://games.crossfit.com/workouts/semifinals/individual/2022"
@@ -127,16 +148,18 @@ def fetch_semis_2022():
             workouts.append(workout)
     return workouts
 
-def fetch_semifinals_workouts():
-    years = range(2021,MAX_YEAR+1)
-    workouts_all = []
-    for year in years:
-        if year == 2022:
-            workouts = fetch_semis_2022()
-        else:
-            workouts = fetch_stage_workout('semifinals', year,max_seq=8)
-        workouts_all.extend(workouts)
-    return workouts_all
+def fetch_semifinals_workouts(year: int):
+    if year == 2022:
+        workouts = fetch_semis_2022()
+    else:
+        workouts = fetch_stage_workout('semifinals', year,max_seq=8)
+    return workouts
+
+def fetch_regionals_workouts(year: int):
+    if year <= 2016:
+        return fetch_stage_workout_old(year, 'regionals')
+    else:
+        return fetch_stage_workout('regionals', year,max_seq=6)
 
 def dump_workouts(workouts: list, file_path: str | Path):
     file_path = Path(file_path)
@@ -145,50 +168,64 @@ def dump_workouts(workouts: list, file_path: str | Path):
         for workout in workouts:
             f.write(json.dumps(workout) + '\n')
 
+def run_scrape(
+    scrape_fn: Callable,
+    comp_type: str,
+    years: list[int], 
+    root_path: str | Path,
+    overwrite: bool = False
+):
+    root_path = Path(root_path)
+    root_path.mkdir(parents=True, exist_ok=True)
+
+    for year in years:
+        fn = root_path / f'{comp_type}_{year}.jsonl'
+        if fn.exists() and not overwrite:
+            continue
+        workouts = scrape_fn(year)
+        if workouts is None:
+            continue
+        dump_workouts(workouts, fn)
+
 if __name__ == '__main__':
-    import os
-    from dotenv import load_dotenv
-    load_dotenv()
+    cf_workouts_path = 'crossfit_workouts/raw'
 
-    MAX_YEAR = 2026
-    OVERWRITE = False
+    run_scrape(
+        scrape_fn=fetch_games_workouts,
+        comp_type='games',
+        years=range(2007,2026),
+        root_path=cf_workouts_path,
+        overwrite=True
+    )
 
-    root_dir = Path(os.getenv('ELITE_COMPETITION_DATA_PATH'))
-    cf_workouts_path = root_dir / 'crossfit' / 'workouts'
-    cf_workouts_path.mkdir(parents=True, exist_ok=True)
+    # run_scrape(
+    #     scrape_fn=fetch_open_workouts,
+    #     comp_type='open',
+    #     years=range(2011,2027),
+    #     root_path=cf_workouts_path,
+    #     overwrite=False
+    # )
 
-    fn = cf_workouts_path / 'open.jsonl'
-    if fn.exists() and not OVERWRITE:
-        print(f'{fn} already exists, skipping')
-    else:
-        print('Fetching open workouts...',end='')
-        open_workouts = fetch_open_workouts()
-        dump_workouts(open_workouts, fn)
-        print('done')
+    # run_scrape(
+    #     scrape_fn=fetch_quarterfinals_workouts,
+    #     comp_type='quarterfinals',
+    #     years=range(2021,2027),
+    #     root_path=cf_workouts_path,
+    #     overwrite=False
+    # )
 
-    fn = cf_workouts_path / 'quarterfinals.jsonl'
-    if fn.exists() and not OVERWRITE:
-        print(f'{fn} already exists, skipping')
-    else:
-        print('Fetching quarterfinals workouts...',end='')
-        quarterfinals_workouts = fetch_quarterfinals_workouts()
-        dump_workouts(quarterfinals_workouts, fn)
-        print('done')
+    # run_scrape(
+    #     scrape_fn=fetch_semifinals_workouts,
+    #     comp_type='semifinals',
+    #     years=range(2021,2026),
+    #     root_path=cf_workouts_path,
+    #     overwrite=False
+    # )
 
-    fn = cf_workouts_path / 'semis.jsonl'
-    if fn.exists() and not OVERWRITE:
-        print(f'{fn} already exists, skipping')
-    else:
-        print('Fetching semis workouts...',end='')
-        semis_workouts = fetch_semifinals_workouts()
-        dump_workouts(semis_workouts, fn)
-        print('done')
-
-    fn = cf_workouts_path / 'games.jsonl'
-    if fn.exists() and not OVERWRITE:
-        print(f'{fn} already exists, skipping')
-    else:
-        print('Fetching games workouts...',end='')
-        games_workouts = fetch_games_workouts()
-        dump_workouts(games_workouts, fn)
-        print('done')
+    # run_scrape(
+    #     scrape_fn=fetch_regionals_workouts,
+    #     comp_type='regionals',
+    #     years=range(2011,2019),
+    #     root_path=cf_workouts_path,
+    #     overwrite=False
+    # )
